b7d1067f119675432677f527b2527c7463763fc3
Viktoria <zaharova181114@gmail.com>
2022-09-07 16:58:41 +0300

    add home

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,35 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$('.objects-slider').slick({
+    slidesToShow: 3,
+    arrows: false,
+    dots: true,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
+
+
+$('.btn-burger').on('click', function () {
+   $('.navbar').fadeToggle();
+});
+
+
+$('.navbar-close').on('click', function (e) {
+    e.preventDefault();
+    $('.navbar').fadeOut();
+});
+
+
+
